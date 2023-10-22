7848d3ae17835ec5d17472a9c330f8969960832c
mariosneo <16939119+mariosneo@users.noreply.github.com>
2023-10-22 20:34:26 +0300

    init oidc terraform github actions (#1)
    * init oidc terraform github actions

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "domain" {
+  description = "value of domain"
+  type = string
+}
+
+variable "sub_domain_record" {
+  default = "dev"
+  description = "value of domain"
+  type = string
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "region" {
+  description = "The aws region. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html"
+  type        = string
+}
+
+variable "record"{
+    description = "route53 record values"
+    type = list(string)
+}
