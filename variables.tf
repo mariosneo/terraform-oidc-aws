6f76d00b76b6d64ad9c998840a69cd50437a693a
mariosneo <16939119+mariosneo@users.noreply.github.com>
2023-10-23 11:09:22 +0300

    init oidc terraform github actions

--- variables.tf
+++ variables.tf
@@ -9,15 +9,15 @@ variable "sub_domain_record" {
   type = string
 }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
+# variable "aws_access_key" {
+#   description = "AWS access key"
+#   type        = string
+# }
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
+# variable "aws_secret_key" {
+#   description = "AWS secret key"
+#   type        = string
+# }
 
 variable "region" {
   description = "The aws region. https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html"
